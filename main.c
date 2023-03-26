b5482e2714a2eed93e9b5df1fe6753b56ec67959
ilya knyazev <ilak9081@gmail.com>
2023-03-26 12:22:10 +0400

    p2-2

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+double rebro; double obyom;
+printf("Enter the length of the edge\n");
+scanf("%lf",&rebro);
+obyom=rebro*rebro*rebro;
+printf("Volume of the cube is = ");
+printf("%6.3lf",obyom);
+}
